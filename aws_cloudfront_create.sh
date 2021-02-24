0e3ac2833bab43f75b96452a5de8b839e6d249a2
Steve Helms <steve@cadenrock.com>
2021-02-24 15:48:44 -0600

    Adding scripts from webapp to central repo.

---
+++ aws_cloudfront_create.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "x$BUCKET" = "x" ]; then
+    echo "No bucket name provided."
+    exit -1
+fi
+
+sed -e "s/{{BUCKET}}/$BUCKET/g" aws-cloudfront-create.json > cloudfront.json
+
+aws cloudfront create-distribution --distribution-config file://cloudfront.json > cloudfront.out 2>&1
+r=$?
+
+grep 'Already exists' cloudfront.out > /dev/null
+
+if [[ $? -eq 0 && $r -eq 254 ]]; then
+    aws cloudfront list-distributions | jq ".DistributionList.Items[] | select (.Origins.Items[].Id | contains(\"$BUCKET\")) | .DomainName" | sed 's/"//g' > url.out
+    echo "Distribution already exists ($(cat url.out)). Continuing..."
+    exit 0
+fi
+
+jq .Distribution.DomainName cloudfront.out | sed 's/\"//g' > url.out
+
+echo "Exiting ($(cat url.out))."
+exit $r
---
+++ aws_cloudfront_delete.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+if [ "x$BUCKET" = "x" ]; then
+    echo "No bucket name provided."
+    exit -1
+fi
+
+echo "Bucket: $BUCKET"
+
+# Search our list of distributions for one matching our PR branch
+aws cloudfront list-distributions | \
+    jq ".DistributionList.Items[] | select (.Origins.Items[].Id | contains(\"$BUCKET\"))" \
+    > cf-list.json
+
+# Setup our ID for later use
+ID=$(jq .Id cf-list.json | sed 's/"//g')
+
+if [ -z $ID ]; then
+    echo "No CloudFront Distribution found."
+    exit 0
+fi
+
+aws cloudfront get-distribution-config --id $ID > cf-distribution.json
+
+# We need the ETag in order to delete the distribution
+ETAG=$(jq .ETag cf-distribution.json | sed 's/"//g')
+
+echo $ETAG
+exit
+# Set enabled to false, capture only DistributionConfig for deletion config
+jq '.DistributionConfig.Enabled = false' cf-distribution.json | \
+    jq .DistributionConfig > cf-disable.json
+
+# First we have to disable the CloudFront distribution
+echo "Disabling CloudFront Distribution $ID"
+aws cloudfront update-distribution --id $ID --distribution-config file://cf-disable.json \
+    --if-match $ETAG > cf-update.json
+
+# We need to wait and recheck the status before deleting.
+while [ 1 ]; do
+    sleep 5
+
+    STATUS=$(aws cloudfront get-distribution --id $ID | jq .Distribution.Status | sed 's/"//g')
+
+    if [ $STATUS = "Deployed" ]; then
+        # Finally we can delete the distribution
+        echo "Deleting CloudFront Distribution $ID with ETag $ETAG"
+        ETAG=$(aws cloudfront get-distribution --id $ID | jq .ETag | sed 's/"//g')
+        aws cloudfront delete-distribution --id $ID --if-match $ETAG
+        exit
+    fi
+
+done
