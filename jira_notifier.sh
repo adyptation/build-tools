e8e5dd1fd676cb9bc3ad7ae3964dc8d34b18459a
Steve Helms <steve@cadenrock.com>
2021-02-26 10:06:03 -0600

    Minor tweak to notifier.

--- jira_notifier.sh
+++ jira_notifier.sh
@@ -18,11 +18,16 @@ if [ "x$ISSUE" = "x" ]; then
     exit 0 # Clean exit so build doesn't break.
 fi
 
+#
+# I have been unable to find a way to send a URL link to Jira, only the text.
+# Neither Markdown or straight HTML seem to work.
+# 2021-Feb-26 -seh
+#
 DATA="{ \"issue\": \"$ISSUE\", \"comment\": \"Preview available at: $URL \" }"
 echo $DATA
 
 
 response=$(curl -s -X POST -H "Content-Type: application/json" --data "$DATA" $FORGE_URL)
 
-echo $response | jq .status | sed 's/^/Status: /'
+echo $response | jq -r .status | sed 's/^/Status: /'
 echo $response | jq -r .body | sed 's/^/Response: /'
