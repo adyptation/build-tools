a99e0bddc13160dc089b4656ef6f834b6ac6aee2
Steve Helms <steve@cadenrock.com>
2021-03-03 08:57:09 -0600

    Create improvements.

--- aws_cloudfront_create.sh
+++ aws_cloudfront_create.sh
@@ -18,19 +18,21 @@ sed -e "s/{{BUCKET}}/$BUCKET/g" aws-cloudfront-create.json | \
 aws cloudfront create-distribution --distribution-config file://cloudfront.json > cloudfront.out 2>&1
 r=$?
 
-echo "result \$r: $r"
+echo "result aws cloudfront \$r: $r"
 grep 'Already exists' cloudfront.out > /dev/null
 r2=$?
-echo "result2 \$r2: $r2"
+echo "result2 grep \$r2: $r2"
 
+url=""
 if [ $r2 -eq 0 -a $r -eq 254 ]; then
-    aws cloudfront list-distributions | jq -r ".DistributionList.Items[] | select (.Origins.Items[].Id | contains(\"$BUCKET\")) | .DomainName"
-    aws cloudfront list-distributions | jq -r ".DistributionList.Items[] | select (.Origins.Items[].Id | contains(\"$BUCKET\")) | .DomainName" > url.out
-    echo "Distribution already exists ($(cat url.out)). Continuing..."
+    url=$(aws cloudfront list-distributions | jq -r ".DistributionList.Items[] | select (.Origins.Items[].Id | contains(\"$BUCKET\")) | .DomainName")
+    if [ -z $url ]; then
+        echo "Distribution already exists. Continuing..."
+    else
+        echo "Distribution already exists ($url). Continuing..."
+    fi
     exit 0
 fi
 
-jq -r .Distribution.DomainName cloudfront.out > url.out
-
-echo "Exiting ($(cat url.out))."
+echo "Exiting ($(jq -r .Distribution.DomainName cloudfront.out))."
 exit $r
